a622a2ee9cd10e2c1afa6f441877d567befcd971
sakib-devxhub <git-sakib2@devxhub.com>
2024-04-03 00:53:52 +0600

    feat: git link

---
+++ git.mjs
@@ -0,0 +1,113 @@
+import puppeteer from "puppeteer"; // created by google core team
+import { setTimeout } from "timers/promises"; //node default promise
+import fs from "fs";
+
+const browser = await puppeteer.launch({
+  headless: false, // make headlefull, no head less
+  defaultViewport: { width: 1920, height: 1080 }, // make screen size
+  // slowMo: 250, //make it slow process
+  userDataDir: "temporary", // keep the created instace so that it wont recreate and make performance better
+});
+
+const page = await browser.newPage();
+
+const githubName = "saakeeb";
+
+const gitUrl = `https://github.com/${githubName}`
+
+await setTimeout(1000);
+await page.goto(gitUrl, { waitUntil: "networkidle2" });
+
+const contribution = await page.waitForSelector(".ContributionCalendar-grid");
+await setTimeout(1000);
+await contribution.scrollIntoView();
+await setTimeout(1000);
+await contribution.screenshot({ path: "contribution.png" });
+
+const name = await page.waitForSelector("h1 span[itemprop='name']");
+const fullName = await page.evaluate((element) => element.textContent, name);
+const nameRegex = /\b[A-Z][a-z]+\s[A-Z][a-z]+\b/;
+const extractedName = fullName.match(nameRegex);
+
+
+const follower = await page.waitForSelector(
+  ".js-profile-editable-area a:nth-child(1) span"
+);
+const followerText = await page.evaluate(
+  (element) => element.textContent,
+  follower
+);
+
+const following = await page.waitForSelector(
+  ".js-profile-editable-area a:nth-child(2) span"
+);
+const followingText = await page.evaluate(
+  (element) => element.textContent,
+  following
+);
+
+const company = await page.waitForSelector(
+  ".js-profile-editable-area .vcard-details .p-org div"
+);
+const companyName = await page.evaluate(
+  (element) => element.textContent,
+  company
+);
+
+const location = await page.waitForSelector(
+  ".js-profile-editable-area .vcard-details .p-label"
+);
+const locationName = await page.evaluate(
+  (element) => element.textContent,
+  location
+);
+
+const userLink = await page.evaluate(
+  () =>
+    document.querySelector(".js-profile-editable-area li:nth-child(4) > a")
+      ?.href
+);
+
+const userTwitter = await page.evaluate(
+  () =>
+    document.querySelector(".js-profile-editable-area li:nth-child(5) > a")
+      ?.href
+);
+
+const userLinkedin = await page.evaluate(
+  () =>
+    document.querySelector(".js-profile-editable-area li:nth-child(6) > a")
+      ?.href
+);
+
+const userLink2 = await page.evaluate(
+  () =>
+    document.querySelector(".js-profile-editable-area li:nth-child(7) > a")
+      ?.href
+);
+
+const userLink3 = await page.evaluate(
+  () =>
+    document.querySelector(".js-profile-editable-area li:nth-child(8) > a")
+      ?.href
+);
+
+const allInformation = [
+  {
+    Name: extractedName[0],
+    Follower: followerText,
+    Following: followingText,
+    Company: companyName,
+    Location: locationName,
+    UserLink: userLink,
+    Twitter: userTwitter,
+    Linkedin: userLinkedin,
+    UserLink2: userLink2,
+    UserLink3: userLink3,
+  },
+];
+
+const jsonData = JSON.stringify(allInformation, null, 2);
+fs.writeFileSync("all_links_data.json", jsonData);
+
+await browser.close();
